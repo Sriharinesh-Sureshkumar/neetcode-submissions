004ea00f21113718bedea177adbb3b809acd9672
Sriharinesh <greathari06@gmail.com>
2026-07-17 21:17:48 +0530

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = 0, n = nums.size(), k = n - 1;
+        while (j <= k){
+            if (nums[j] == 0){
+                swap(nums[i], nums[j]);
+                j++;
+                i++;
+            }
+            else if (nums[j] == 2){
+                swap(nums[j], nums[k]);
+                k--;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+};
